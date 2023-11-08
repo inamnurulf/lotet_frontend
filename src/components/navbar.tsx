a3e1e1298e2da69b1c0e6b6c824fbaa5ba694501
inamnurul <aam.inam7310@gmail.com>
2023-11-09 00:52:10 +0700

    feat: responsive styling navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,21 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
-const Navbar = () => {
+const Navbar = (defaultform : boolean) => {
   const [scrollY, setScrollY] = useState(0);
   const [isFaded, setIsFaded] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const fadeEffect = {
     opacity: isFaded ? 1 : 0,
     transition: "opacity 0.1s ease-in-out",
+  };
+
+  const handleNavbar = () => {
+    setIsOpen(!isOpen);
   };
 
   const handleScroll = () => {
@@ -29,25 +36,50 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-full bg-blue-300 h-[5vh]"></div>
+      <div className="w-full md:h-[5vh]"></div>
       <div className="sticky top-0">
         <div
-          className="bg-red-300 absolute w-full h-[70px] top-0"
+          className=" absolute w-full h-[70px] top-0 bg-white"
           style={fadeEffect}
         ></div>
-        <div className="relative w-[95vw] md:w-[90vw] mx-auto p-2 h-[70px] bg-red-300 rounded-full top-0 flex justify-between items-center">
-            <div>
-                <Image src={'./Logo.svg'} width={155} alt="Logo" height={64} className="max-h-[40px] md:max-h-[50px] w-[150px]"/>
+        <div className="relative bg-white w-[full] md:w-[90vw] mx-auto p-2 h-[70px]  md:rounded-full top-0 flex justify-between items-center">
+          <div>
+            <Image
+              src={"./Logo.svg"}
+              width={155}
+              alt="Logo"
+              height={64}
+              className="max-h-[40px] md:max-h-[50px] w-[150px]"
+            />
+          </div>
+          <div className="justify-between hidden md:flex">
+            <a href="/" className="text-xl mx-3 hover:bg-primary hover:text-white px-3 py-2 rounded rounded-xl font-semibold">Home</a>
+            <a href="/kerjapraktik" className="text-xl mx-3 hover:bg-primary hover:text-white px-3 py-2 rounded rounded-xl font-semibold">Kerja Praktik</a>
+            <a href="/seminar" className="text-xl mx-3 hover:bg-primary hover:text-white px-3 py-2 rounded rounded-xl font-semibold">Seminar</a>
+          </div>
+          <a href="/signIn" className="w-[150px] text-center justify-center hidden md:flex ">
+            <div className="hover:bg-primary hover:text-white px-3 py-2 rounded rounded-xl font-semibold text-xl">
+            Sign In
             </div>
-            <div className="flex justify-between">
-                <div className="mx-3">Home</div>
-                <div className="mx-3">Kerja Praktik</div>
-                <div className="mx-3">Seminar</div>
-            </div>
-            <div className="w-[150px] text-center">
-                Sign In 
-            </div>
+          </a>
+          <div className="text-3xl md:hidden px-3 ">
+            {isOpen ? (
+              <HiOutlineMenuAlt3 onClick={handleNavbar} />
+            ) : (
+              <AiOutlineClose onClick={handleNavbar} />
+            )}
+          </div>
         </div>
+        {!isOpen ? (
+          <div className="w-full items-center bg-white">
+            <div className=" z-[100] w-full md:w-[90vw] text-right md:hidden flex justify-center flex-col items-center p-3">
+              <div className="mx-3 hover:bg-primary hover:text-white w-full p-3 border border-2">Home</div>
+              <div className="mx-3 hover:bg-primary hover:text-white w-full p-3 border border-2">Kerja Praktik</div>
+              <div className="mx-3 hover:bg-primary hover:text-white w-full p-3 border border-2">Seminar</div>
+              <div className="mx-3 hover:bg-primary hover:text-white w-full p-3 border border-2">Sign In</div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </>
   );
