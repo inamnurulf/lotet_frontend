b0cb4cfed6026382b9b597350844c842e0809c3d
Yasmine  'Arfa Zahira <134650080+yas-mine-az@users.noreply.github.com>
2023-11-17 06:18:46 +0700

    update kp page

--- src/app/kerjapraktik/page.tsx
+++ src/app/kerjapraktik/page.tsx
@@ -1,9 +1,45 @@
+import KPCard from '@/components/atom/kpCard'
+import Navbar from '@/components/navbar'
+import SearchBar from '@/components/searchBar'
 import React from 'react'
 
 const KerjaPraktik = () => {
   return (
-    <div>
-      This is kerja praktik page
+    <div className='flex items-center flex-col overflow-hidden bg-primary min-h-screen'>
+      <Navbar defaultform = {false}></Navbar>
+      
+      <div className='mt-8 w-1/3 min-w-fit md:min-w-0 flex justify-center'>
+        <SearchBar></SearchBar>
+      </div>
+
+      <div className='mx-12 flex justify-center'>
+        <div className='grid grid-cols-1'>
+
+          <KPCard
+          title='Title of Position from KP'
+          date='14-January-2003'
+          location='Jakarta'
+          company='PT ABCD efghijkl'
+          />
+
+          <KPCard
+          title='Title of Position from KP'
+          date='14-January-2003'
+          location='Jakarta'
+          company='PT ABCD efghijkl'
+          />
+
+          <KPCard
+          title='Title of Position from KP'
+          date='14-January-2003'
+          location='Jakarta'
+          company='PT ABCD efghijkl'
+          />
+
+          </div>
+      </div>
+      
+      
     </div>
   )
 }
