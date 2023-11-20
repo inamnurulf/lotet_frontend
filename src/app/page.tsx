c4ac960033daec457cf7cacca7eb71e2f94aff62
inamnurul <aam.inam7310@gmail.com>
2023-11-20 07:18:03 +0700

    fix: removing handle authorization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="w-screen h-screen bg-primary justify-center items-center flex flex-col">
+      <section className="min-w-screen min-h-screen bg-primary justify-center items-center flex flex-col backgroundsection">
         <div className="w-96 h-52 text-center text-white text-4xl font-bold">
           Welcome to
           <Image
@@ -15,15 +15,15 @@ export default function Home() {
           />
           
           </div>
-        <div className="mt-8 transform transition-transform hover:scale-105">
+        <div className="mt-8 transform transition-transform hover:scale-105 bg-secondary px-5 py-2 rounded-xl hover:bg-white text-white hover:text-secondary">
           <a
             href="/home"
-            className="text-white text-xl font-semibold hover:underline"
+            className="text-xl font-semibold"
           >
             Start
           </a>
         </div>
-      </div>
+      </section>
     </main>
     
   )
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='bg-white w-screen min-h-[20vh]'>
+      
+    </section >
+  )
+}
+
+export default Footer
