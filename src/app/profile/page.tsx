d3b9e47a4bf8e5f9be275f148518f22d9c4d8057
BonarSs <bonaruli42@gmail.com>
2023-12-02 22:40:13 +0700

    update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,49 +4,69 @@ import HistoryCard from "@/components/historycard";
 
 const Profile = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="bg-black h-[70px]">
-                <Navbar></Navbar>
-            </div>
-            <div className="flex-1 grid grid-cols-12 ">
-                {/* Container informasi profile (di kiri) */}
-                <div className="col-span-3 bg-red-300 ">
+        <div className="h-screen flex flex-col">
+      {/* Top part */}
+      <div className="h-[70px] bg-blue-500">
+        {/* Content for the top part */}
+        <Navbar/>
+      </div>
 
-                </div>
-
-                {/* Container kanan isi list seminar & kp */}
-                <div className="col-span-9 grid grid-rows-2">
-                    <div className="flex flex-col items-center row-span-1 bg-secondary max-h-full ">
-                        <div className="flex flex-col w-[85%] h-[55vh]">
-                            <h1 className="text-2xl font-bold text-white my-3">Seminar</h1>
-                            <div className="flex-1 bg-primary rounded-md p-3 w-full overflow-y-auto scrollbar">
-                                <HistoryCard/>
-                                <HistoryCard/>
-                                <HistoryCard/>
-                                <HistoryCard/>
-                                <HistoryCard/>
-                            </div>
-                            <div className="flex flex-row-reverse my-3">
-                                <button className="w-[10%] bg-[#add03f] rounded-md hover:bg-[#76970f]">
-                                    +add
-                                </button>
-                            </div>
-
-                        </div>
-                        {/* Container List Seminar */}
-
-                        {/* Container tombol add */}
-                        <div className="flex justify-end mx-8">
-
-                        </div>
-
-                    </div>
-                    <div className="row-span-1 bg-primary">
-
-                    </div>
-                </div>
-            </div>
+      {/* Bottom part */}
+      <div className="flex flex-1">
+        {/* Left part */}
+        <div className="flex flex-row w-1/5 bg-green-500">
+          {/* Content for the left part */}
+          <div className="flex items-end rounded-xl mx-auto h-[40px] w-[70%] bg-red-950 ">
+            
+          </div>
+          <div className="flex items-end rounded-xl mx-auto h-[40px] w-[70%] bg-red-950 ">
+            
+          </div>
+         
         </div>
+
+        {/* Right part */}
+        <div className="flex flex-col flex-1">
+          {/* Right-top */}
+          <div className="flex-1 bg-primary">
+            {/* Content for the right-top */}
+            <h1 className="mx-20 my-3 font-bold text-white">Seminar</h1>
+            <div className="rounded-xl w-[85%] h-[70%] shadow-md mx-auto">
+                <div className="bg-primary rounded-md mx-auto p-3 max-w-full h-[31vh] overflow-y-auto scrollbar">
+                    <HistoryCard/>
+                    <HistoryCard/>
+                    <HistoryCard/>
+                    <HistoryCard/>
+                </div>
+            </div>
+                <div className="flex justify-end mx-8">
+                    <button className="mx-11 mt-1 w-[50px] h-[25px] bg-[#add03f] rounded-md  hover:bg-[#76970f]">
+                        +add
+                    </button>
+                </div>
+          </div>
+
+          {/* Right-bottom */}
+          <div className="flex-1 bg-primary">
+            {/* Content for the right-bottom */}
+            <h1 className="mx-20 my-3 font-bold text-white">Kerja Praktik</h1>
+            <div className="rounded-xl w-[85%] h-[70%] shadow-md mx-auto">
+                <div className="bg-primary rounded-md mx-auto p-3 max-w-full h-[31vh] overflow-y-auto scrollbar">
+                    <HistoryCard/>
+                    <HistoryCard/>
+                    <HistoryCard/>
+                    <HistoryCard/>
+                </div>
+            </div>
+                <div className="flex justify-end mx-8">
+                    <button className="mx-11 mt-1 w-[50px] h-[25px] bg-[#add03f] rounded-md  hover:bg-[#76970f]">
+                        +add
+                    </button>
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
 }
 
