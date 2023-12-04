b27544daa13b16f16f1dac7b93948090f225285e
inamnurul <aam.inam7310@gmail.com>
2023-12-05 00:23:42 +0700

    fix: styling profile

--- src/components/historycard.tsx
+++ src/components/historycard.tsx
@@ -11,13 +11,13 @@ const HistoryCard = ({
   temp
 }:any) => {
   return (
-    <div className="text-primary bg-white p-4 my-3 rounded-lg md:max-w-[95%] mx-auto  overflow-hidden">
+    <div className="text-primary bg-white hover:bg-[#add03f] hover:text-white p-2 my-3 rounded-lg md:max-w-[95%] mx-auto overflow-hidden">
       <div className="flex justify-between w-full">
         <div className="max-w-[100%]">
-          <h2 className="md:text-xl font-bold">{display_text}</h2>
+          <h2 className="md:text-base">{display_text}</h2>
 
         </div>
-        <div className="flex md:text-3xl items-center">
+        <div className="flex md:text-2xl items-center">
           <div></div>
           <AiOutlineDelete
             className="hover:bg-primary hover:text-white rounded-md"
