18061e07a99c8102fab87687a9e735186d1477e3
inamnurul <aam.inam7310@gmail.com>
2023-12-04 21:36:07 +0700

    add: minor styling

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,10 +3,18 @@ import Carousell from "@/components/carousell";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import React, { useRef, useEffect } from "react";
+import { FaArrowDown } from "react-icons/fa6";
 import Typed from "typed.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
   const typedRef = useRef<any>(null);
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+    });
+  }, []);
 
   useEffect(() => {
     const options = {
@@ -28,8 +36,8 @@ const Home = () => {
       <Navbar defaultform={false} />
       <div className="md:flex">
         <div className="md:w-[50vw] flex flex-col justify-center items-start min-h-screen">
-          <div className="left-[15%] relative">
-            <div className="flex items-end">
+          <div className="left-[15%] relative" >
+            <div className="flex items-end" data-aos="fade-right">
               <div className="text-white text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold">
                 Pengen
               </div>
@@ -37,12 +45,15 @@ const Home = () => {
                 <span id="animated-text" className="text-secondary"></span>
               </div>
             </div>
-            <div className="text-white text-xl py-3">
+            <div className="text-white text-xl py-3" data-aos="fade-left">
               Tetep kudu cari KP dulu !
             </div>
           </div>
+          <div className="w-full text-center absolute" data-aos="fade-right">
+            <FaArrowDown className="text-white animate-bounce md:hidden mx-auto mt-[120vw]"/>
+          </div>
         </div>
-        <div className="bg-secondary w-screen md:w-[50vw] min-h-screen flex items-center">
+        <div className="bg-secondary w-screen md:w-[50vw] min-h-screen flex items-center" id="whatsnew">
           <div className="mx-auto">
             <div className="text-white text-2xl px-7 py-2 font-semibold">
               Whats New?
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <section className='bg-white w-screen min-h-[20vh]'>
-      <div className='w-[50vw] h-[20vh] flex items-center '>
+      <div className='md:w-[50vw] h-[20vh] flex items-center '>
         <Image src={"./Logo.svg"} width={155} height={64} alt="Logo" className='h-[30%]'/>
         <p className='text-[8px]/[8px] p-2 md:max-w-[50%]'>WebApp yang ditujukan untuk membantu mahasiswa saling bertukar informasi mengenai KP dan Seminar yang akan diselenggarakan</p>
       </div>
