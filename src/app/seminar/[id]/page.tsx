284e602b741f132792e33e02970d979ebc50b941
egasetiawan <ega.rizky.setiawan@mail.ugm.ac.id>
2023-12-07 05:58:39 +0700

    minor change seminar

--- src/app/seminar/[id]/page.tsx
+++ src/app/seminar/[id]/page.tsx
@@ -1,28 +1,22 @@
-import React from 'react'
-import SeminarDescription from '@/components/seminarDescription'
-import Navbar from '@/components/navbar'
+'use client'
+import { useRouter } from 'next/router';
+import SeminarDescription from '@/components/seminarDescription';
+import Navbar from '@/components/navbar';
 import SeminarInfo from '@/components/seminarInfo';
 
-interface Seminar{
-  _id: string;
-  user_id: string;
-  title: string;
-  details: string;
-  image: string;
-  eventTime: string;
-  category: string[];
-}
-
-const SeminarDetails = () => {
+const SeminarDetails = ({params}:any) => {
+  const {id} = params
   return (
     <div className='flex flex-col bg-primary items-center overflow-hidden min-w-fit min-h-screen'>
-      <Navbar defaultform = {false}/>
+      <Navbar defaultform={false} />
+      <div>{id}</div>
       <div className='justify-center grid grid-cols-1 md:grid-cols-2 gap-8 mt-32 mx-8'>
-        <SeminarDescription/>
-        <SeminarInfo></SeminarInfo>
+        {/* Pass the ID to the components that require it */}
+        <SeminarDescription />
+        <SeminarInfo />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SeminarDetails
+export default SeminarDetails;
