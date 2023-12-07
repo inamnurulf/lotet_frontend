b9f8b41aff3acf5d31c40495805dbc561c845c9d
inamnurul <aam.inam7310@gmail.com>
2023-12-08 05:25:55 +0700

    feat: solid button functionality

--- src/components/atom/solidButton.tsx
+++ src/components/atom/solidButton.tsx
@@ -1,17 +1,46 @@
 'use client';
+import React from 'react';
+import { toast } from "react-toastify";
 
-interface solidButtonProps{
-  name: string
+
+interface SolidButtonProps {
+  name: string;
 }
 
-const SolidButton: React.FC<solidButtonProps> = ({name}) =>{
-  return(
-    <div className="cursor-default">
-      <button className="text-white bg-primary py-px px-2 rounded-lg font-semibold select-none hover:shadow-mds">
+const SolidButton: React.FC<SolidButtonProps> = ({ name }) => {
+  const handleClick = () => {
+    if (name === 'Share') {
+      navigator.clipboard.writeText(window.location.href)
+        .then(() => {
+          toast("Link copied on clipboard", {
+            hideProgressBar: false,
+            autoClose: 2000,
+            type: "success",
+            theme: "colored",
+          });
+        })
+        .catch((error) => {
+          toast("Failed to copy URL to clipboard", {
+            hideProgressBar: false,
+            autoClose: 2000,
+            type: "error",
+            theme: "colored",
+          });
+          console.error('Failed to copy URL to clipboard', error);
+        });
+    }
+  };
+
+  return (
+    <div className="cursor-pointer">
+      <button
+        className="text-white bg-primary py-px px-2 rounded-lg font-semibold select-none hover:shadow-mds"
+        onClick={handleClick}
+      >
         {name}
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default SolidButton;
